2d51d423719cbcc50386d3064dfda8bb6b79375c
Amodh Dhakal <amodhakal@gmail.com>
2026-01-12 13:46:45 -0500

    feat: add the todoist oauth workflow

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -13,10 +13,39 @@ export const auth = betterAuth({
         {
           providerId: "todoist",
           clientId: process.env.TODOIST_ID as string,
-          clientSecret: process.env.TODIST_SECRET as string,
+          clientSecret: process.env.TODOIST_SECRET as string,
           authorizationUrl: process.env.TODOIST_OAUTH_URL,
           tokenUrl: process.env.TODOIST_TOKEN_URL,
+          redirectURI: process.env.TODOIST_REDIRECT_URL,
           scopes: ["data:read_write"],
+          getUserInfo: async (token) => {
+            try {
+              const res = await fetch(
+                process.env.TODOIST_USERINFO_URL as string,
+                {
+                  headers: { Authorization: `Bearer ${token.accessToken}` },
+                },
+              );
+
+              if (!res.ok) {
+                console.error(res);
+                return null;
+              }
+
+              const profile = await res.json();
+
+              return {
+                id: profile.id, // string
+                name: profile.full_name || undefined,
+                email: profile.email || undefined,
+                image: profile.avatar_medium || undefined,
+                emailVerified: true, // Todoist doesn’t provide verified, set true
+              };
+            } catch (err) {
+              console.error("Failed to fetch Todoist user info", err);
+              return null;
+            }
+          },
         },
       ],
     }),
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 
 const { DATABASE_URL } = process.env;
 if (!DATABASE_URL) {
   throw new Error("Missing DATABASE_URL in .env");
 }
 
-export const db = drizzle(DATABASE_URL);
+export const db = drizzle(DATABASE_URL, { schema });
