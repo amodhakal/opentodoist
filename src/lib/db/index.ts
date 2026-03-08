037c3a96b9f6e6b57b284dc0fcb8c8f0377ab472
Amodh Dhakal <amodhakal@gmail.com>
2026-03-08 19:44:29 -0400

    chore: reset project

--- src/lib/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
-
-const { DATABASE_URL } = process.env;
-if (!DATABASE_URL) {
-  throw new Error("Missing DATABASE_URL in .env");
-}
-
-export const db = drizzle(DATABASE_URL, { schema });
--- src/lib/db/schema/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./auth";
-export * from "./process";
-export * from "./todoItem";
